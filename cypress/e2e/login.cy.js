fd6a4a7d6859f84ab13581602706b66c2e46ede4
rahmansopiann <rahmansopian6@gmail.com>
2023-03-10 14:19:11 +0700

    v1.2

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,9 @@
 describe("Login Scenario saucedemo", () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
   // Define positif case
   it("Login successfully with valid credentials", () => {
-    cy.visit('/')
     cy.get("#user-name").type("standard_user");
     cy.get("#password").type("secret_sauce");
     cy.get("#login-button").click();
@@ -10,7 +12,6 @@ describe("Login Scenario saucedemo", () => {
   });
   // Define negatif case
   it("Shows error message with invalid credentials", () => {
-    cy.visit('/')
     cy.get("#user-name").type("invalid_user");
     cy.get("#password").type("invalid_password");
     cy.get("#login-button").click();
--- cypress/e2e/loginPageObject.cy.js
+++ cypress/e2e/loginPageObject.cy.js
@@ -1,24 +1,10 @@
 import LoginPage from "../pageObjects/loginPage";
 
-let username = Cypress.env('validUsername');
-let password = Cypress.env('validPassword');
+let username = Cypress.env("validUsername");
+let password = Cypress.env("validPassword");
 
 describe("Login Scenario saucedemo", () => {
-  
-  // Define positif case
-  it("Login successfully with valid credentials", () => {
-    LoginPage.visit();
-    LoginPage.fillUsername(username);
-    LoginPage.fillPassword(password);
-    LoginPage.clickLoginButton();
-    LoginPage.isLoggedIn();
-  });
-  // Define negatif case
-  it("Shows error message with invalid credentials", () => {
-    LoginPage.visit();
-    LoginPage.fillUsername("invalid_user");
-    LoginPage.fillPassword("invalid_password");
-    LoginPage.clickLoginButton();
-    LoginPage.getErrorMessage().should("be.visible");
-  });
+  beforeEach(() => {});
+  it("Login successfully with valid credentials", () => {});
+  it("Shows error message with invalid credentials", () => {});
 });
